cba782e37a730056747c7ea3190a499b3250fc00
etar125 <inniesharpsoftware@gmail.com>
2026-05-02 15:07:18 +0300

    config.mk: C99 -> C89

--- config.mk
+++ config.mk
@@ -1,8 +1,8 @@
-VERSION = "0.1.0"
+VERSION = 0.1.0
 
 PREFIX = /usr
 
-ECFLAGS = -Iinclude -std=c99 -pedantic -Wall -le1l -DVERSION=\"$(VERSION)\" $(CFLAGS)
+ECFLAGS = -Iinclude -std=c89 -pedantic -Wall -le1l -DVERSION=\"$(VERSION)\" $(CFLAGS)
 
 CC = cc
 AR = ar
